935dcfe883f2b445242b6259806dbf3b82c2a3c1
progvar <prog.varadi@gmail.com>
2016-10-02 21:21:47 +0200

    Init setup of the webpack.config and the package.json

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	entry: './src',
+	output: {
+		path: './build',
+		filename: 'bundle.js'
+	},
+	module: {
+		loader: {
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loader: 'babel-loader',
+			query: {
+				presets: ['es2017', 'es2016', 'es2017', 'react']
+			}
+		}
+	}
+}
