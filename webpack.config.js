f3253c9e56a3a33bf0c831cb768abefb1eea20be
progvar <prog.varadi@gmail.com>
2016-10-02 22:09:16 +0200

    Fix presets

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
 			exclude: /node_modules/,
 			loader: 'babel-loader',
 			query: {
-				presets: ['es2017', 'es2016', 'es2017', 'react']
+				presets: ['es2017', 'es2016', 'es2015', 'react']
 			}
 		}
 	}
