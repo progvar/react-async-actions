cc49591e2d9189dc25cd0eb549671d44cb098c4c
progvar <prog.varadi@gmail.com>
2016-12-04 22:28:41 +0100

    Remove dummy logout code

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -32,11 +32,6 @@ function parseResponse(res) {
 }
 
 
-function logoutUser(uid) {
-    // some more magic
-}
-
-
 export function login(username, password) {
     return async dispatch => {
         try {
@@ -48,17 +43,3 @@ export function login(username, password) {
         }
     }
 }
-
-
-/*export function logout(uid) {
-    return async dispatch => {
-        try {
-            dispatch({ type: 'LOGOUT_ATTEMPT' })
-            await logoutUser(uid);
-            dispatch({ type: 'LOGOU_SUCCESS', payload: false })
-        } catch(err) {
-            dispatch({ type: 'LOGOUT_FAILED' })
-            console.log(err);
-        }
-    }
-}*/
--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -10,9 +10,6 @@ export default function rootReducer(state = initState, action) {
             return state;
         case 'LOGIN_SUCCESS':
             return { loggedIn: action.payload };
-        case 'LOGIN_FAILED':
-            return state;
-        // dummy logout reducer will be the same, kinda
         default:
             return state;
     }
